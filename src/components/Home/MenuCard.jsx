83e1a592ad1358ffaf7b52c71448194ed22f4923
Manu M Benakal <manubenakal@gmail.com>
2023-05-28 21:36:49 +0530

    Added Contact compo

---
+++ src/components/Home/MenuCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+const MenuCard = ({ ItemNum, burgerSrc, price, title, handler, delay = 0 }) => {
+  return (
+    <motion.div
+      className='menucard'
+      initial={{
+        x: '-100%',
+        opacity: 0,
+      }}
+      whileInView={{
+        x: 0,
+        opacity: 1,
+      }}
+      transition={{
+        delay,
+      }}
+    >
+      <div>Item {ItemNum}</div>
+      <main>
+        <img src={burgerSrc} alt={ItemNum}></img>
+        <h5>₹{price}</h5>
+        <p>{title}</p>
+        <button onClick={() => handler(ItemNum)}>Buy Now</button>
+      </main>
+    </motion.div>
+  );
+};
+
+export default MenuCard;
